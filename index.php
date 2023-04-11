339d1c9493a23b9676f4c8adb1e069605232af6b
daisyleon <daisyleonhart@gmail.com>
2023-04-11 09:31:24 -0400

    update changed

--- index.php
+++ index.php
@@ -28,6 +28,29 @@ function generate_useragent()
     return $random_user_agent;
 }
 
+function connect_db()
+{
+    // Database credentials
+    $username = "doadmin";
+    $password = "AVNS_8vHG9GOXC3YYJuUy7ST";
+    $host = "db-mysql-nyc1-75307-do-user-13792118-0.b.db.ondigitalocean.com";
+    $port = "25060";
+    $database = "defaultdb";
+    $sslmode = "REQUIRED";
+
+    // Create connection
+    $conn = mysqli_init();
+    mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+    mysqli_real_connect($conn, $host, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+
+    // Check connection
+    if (mysqli_connect_errno()) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Connection successful, continue with your queries here
+}
+
 // This "onboarding_csrf" function is working to get the csrf token to be used in the next request
 function onboarding_csrf()
 {
@@ -54,7 +77,7 @@ function onboarding_csrf()
     if ($response === false) {
         $data = [
             'error' => TRUE,
-            'message' => curl_error($curl)
+            'message' => curl_error($curl) . ' | STEP : Onboarding CSRF'
         ];
     } else {
         if ($http_code == 200) {
@@ -66,7 +89,7 @@ function onboarding_csrf()
         } else {
             $data = [
                 'error' => TRUE,
-                'message' => $http_code
+                'message' => $http_code . ' | STEP : Onboarding CSRF'
             ];
         }
     }
@@ -79,9 +102,11 @@ function onboarding_csrf()
 $onboarding_requirements = onboarding_csrf();
 
 // if there is an error in getting the csrf, it will print the error message on the browser, or else the request will continue
-if ($onboarding_requirements['error'] === TRUE) {
-    echo json_encode($onboarding_requirements['message']);
-} else {
-    print_r($onboarding_requirements);
-}
+//if ($onboarding_requirements['error'] === TRUE) {
+//    echo json_encode($onboarding_requirements['message']);
+//} else {
+//    print_r($onboarding_requirements);
+//}
+
+connect_db();
 
